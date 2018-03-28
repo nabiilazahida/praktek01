c78d7e3c278afe2943ad12c0088cf161826d70e4
nabiila zahida <35827020+nabiilazahida@users.noreply.github.com>
2018-03-28 18:07:34 +0800

    Membuat atribut dan behaivor

--- src/praktek01/Butik.java
+++ src/praktek01/Butik.java
@@ -10,5 +10,19 @@ package praktek01;
  * @author Windows 10 Pro
  */
 public class Butik {
+    String merek;
+    String bahan;
+    String warna;
+    String ukuran;
+    String harga;
     
+    void cetakInfo (){
+        System.out.println("Merek   :"+merek);
+        System.out.println("Bahan   :"+bahan);
+        System.out.println("Warna   :"+warna);
+        System.out.println("Ukuran   :"+ukuran);
+        System.out.println("Harga   :"+harga);
+        
+        
+    }
 }
